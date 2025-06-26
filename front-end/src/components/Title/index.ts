4735c1275aa4c6c1fd908eac0caef3709e94161b
Luso Netto <lusonetto137@outlook.com>
2025-06-25 22:18:38 -0300

    refactor: change Tilte component

--- front-end/src/components/Title/index.ts
+++ front-end/src/components/Title/index.ts
@@ -4,10 +4,10 @@ export const Title = styled.h1`
   position: relative;
   padding: 0;
   margin: 0;
-  font-family: "Roboto";
+  font-family: 'Roboto';
   font-weight: 300;
   font-size: 40px;
   color: #080808;
   text-transform: uppercase;
   font-weight: bold;
-`;
+`
