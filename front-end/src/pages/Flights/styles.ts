1095b398f040e3a72923138afbe5910ebb0d5435
Luso Netto <lusonetto137@outlook.com>
2025-06-25 21:41:08 -0300

    feat: add flight container styles

---
+++ front-end/src/pages/Flights/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const FlightsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+`
