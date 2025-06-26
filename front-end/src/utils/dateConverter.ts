75f9343951f083a44a8d7651da5b05771d227bcb
Luso Netto <lusonetto137@outlook.com>
2025-06-25 22:15:46 -0300

    refactor: run eslint and prettier

--- front-end/src/utils/dateConverter.ts
+++ front-end/src/utils/dateConverter.ts
@@ -1,7 +1,7 @@
 export const convertDataBaseDateToFormDate = (departureDateTime: Date) => {
-  const date = new Date(departureDateTime);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-  return year + '-' + month + '-' + day;
-};
+  const date = new Date(departureDateTime)
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const year = date.getFullYear()
+  return year + '-' + month + '-' + day
+}
