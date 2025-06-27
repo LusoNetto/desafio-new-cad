f2f08ebae23d0d484e60a8297a196dd3a5a104fd
Luso Netto <lusonetto137@outlook.com>
2025-06-27 09:45:52 -0300

    feat: add navigation and error handling to the app

--- front-end/src/components/GenericError/styles.ts
+++ front-end/src/components/GenericError/styles.ts
@@ -6,6 +6,9 @@ export const GenericErrorContainer = styled.div`
   align-items: center;
   justify-content: center;
 
+  max-width: 1200px;
+  margin: 0 auto;
+
   padding: 1rem;
   border-radius: 0.5rem;
   border: 2px solid ${({ theme }) => theme.COLORS.ERROR_MAIN};
