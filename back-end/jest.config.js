b9380b54acc2e23063e516b0ab953a56eec1734f
Luso Netto <lusonetto137@outlook.com>
2025-06-26 23:16:47 -0300

    refactor(jest): convert module.exports to ES module syntax

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -1,7 +1,5 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  roots: ['<rootDir>/src'],
-};
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const testMatch = ['**/__tests__/**/*.test.ts'];
+export const moduleFileExtensions = ['ts', 'js', 'json', 'node'];
+export const roots = ['<rootDir>/src'];
